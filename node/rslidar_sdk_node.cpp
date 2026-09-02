854bab8b69cd291d983825b553d41ef89eb2feed
xxlun-tech <xxlun-shi@outlook.com>
2026-09-02 21:30:09 +0800

    ROS2

--- node/rslidar_sdk_node.cpp
+++ node/rslidar_sdk_node.cpp
@@ -35,29 +35,17 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <rs_driver/macro/version.hpp>
 #include <signal.h>
 
-#ifdef ROS_FOUND
-#include <ros/ros.h>
-#include <ros/package.h>
-#elif ROS2_FOUND
 #include <rclcpp/rclcpp.hpp>
-#endif
 
 using namespace robosense::lidar;
 
-#ifdef ROS2_FOUND
 std::mutex g_mtx;
 std::condition_variable g_cv;
-#endif
 
 static void sigHandler(int sig)
 {
   RS_MSG << "RoboSense-LiDAR-Driver is stopping....." << RS_REND;
-
-#ifdef ROS_FOUND
-  ros::shutdown();
-#elif ROS2_FOUND
   g_cv.notify_all();
-#endif
 }
 
 int main(int argc, char** argv)
@@ -72,11 +60,7 @@ int main(int argc, char** argv)
   RS_TITLE << "**********                                    **********" << RS_REND;
   RS_TITLE << "********************************************************" << RS_REND;
 
-#ifdef ROS_FOUND
-  ros::init(argc, argv, "rslidar_sdk_node", ros::init_options::NoSigintHandler);
-#elif ROS2_FOUND
   rclcpp::init(argc, argv);
-#endif
 
   std::string config_path;
 
@@ -88,21 +72,13 @@ int main(int argc, char** argv)
 
    config_path += "/config/config.yaml";
 
-#ifdef ROS_FOUND
-  ros::NodeHandle priv_hh("~");
-  std::string path;
-  priv_hh.param("config_path", path, std::string(""));
-#elif ROS2_FOUND
   std::shared_ptr<rclcpp::Node> nd = rclcpp::Node::make_shared("param_handle");
   std::string path = nd->declare_parameter<std::string>("config_path", "");
-#endif
 
-#if defined(ROS_FOUND) || defined(ROS2_FOUND)
   if (!path.empty())
   {
     config_path = path;
   }
-#endif
 
   YAML::Node config;
   try
@@ -125,12 +101,8 @@ int main(int argc, char** argv)
 
   RS_MSG << "RoboSense-LiDAR-Driver is running....." << RS_REND;
 
-#ifdef ROS_FOUND
-  ros::spin();
-#elif ROS2_FOUND
   std::unique_lock<std::mutex> lck(g_mtx);
   g_cv.wait(lck);
-#endif
 
   return 0;
 }
